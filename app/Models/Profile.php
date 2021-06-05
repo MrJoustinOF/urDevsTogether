95742af212cc198c91f747865fd208362dd487d1
Joustin Ortiz <ortizjoustin@gmail.com>
2021-06-05 10:48:37 -0500

    Change on ProfileController

--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -9,6 +9,11 @@ class Profile extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        'description',
+        'image'
+    ];
+
     public function user()
     {
         return $this->belongsTo(User::class, 'user_id');
